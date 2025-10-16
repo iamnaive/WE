29c54af75400b71f4d025478eaca4b431ed61ed4
naive <36657473+iamnaive@users.noreply.github.com>
2025-10-16 13:16:45 +1000

    Delete app directory

--- app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    // На случай, если какая-то транзитивная зависимость попытается импортировать RN-модуль:
-    config.resolve.alias['@react-native-async-storage/async-storage'] = false;
-    return config;
-  },
-};
-module.exports = nextConfig;
